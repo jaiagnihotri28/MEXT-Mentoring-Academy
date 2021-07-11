36142bdad627bc2d9ebca88a3485d2d9bf9b878d
Jai Agnihotri <jai.ranimukesh12@gmail.com>
2021-07-11 14:55:13 +0530

    first commit

---
+++ assets/js/myjs.js
@@ -0,0 +1,20 @@
+let discountbtn = document.querySelector(".discount");
+let profilepic = document.querySelector(".profilepic");
+
+discountbtn.addEventListener("click",function(e){
+    
+    alert("We have a combo package of 39$ each for 2 people or more!");
+})
+
+// let arrayofpp = [];
+// arrayofpp.push("images/pp2.jpg")
+// arrayofpp.push("images/pp3.jpg")
+// let i=0;
+// while(true){
+//     profilepic.setAttribute("src",arrayofpp[i]);
+//     setInterval(5000);
+//     i++;
+//     if(i==arrayofpp.length-1){
+//         i=0;
+//     }
+// }
